be828a86de0c531e9bc57466bed91c4d392edcda
Makeda P Mckune <makedamckune@gmail.com>
2019-10-07 23:26:59 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+ puts "Hi, my name is #{name}."
+end
+introduction = ("makeda")
+
+def introduction_with_language(name, language)
+  puts "Hi, I'm #{name} and I'm currently learning #{language}."
+end
+introduction_with_language = ("Makeda", "coding")
+  
