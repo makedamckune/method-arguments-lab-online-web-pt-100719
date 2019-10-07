4e5a9262ca319c6882b49b3d5579b1f6c96f8bda
Makeda P Mckune <makedamckune@gmail.com>
2019-10-07 23:25:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,8 @@ def introduction(name)
 end
 introduction = ("makeda")
 
-def introduction(name, language)
+def introduction_with_language(name, language)
+  puts "Hi, I'm #{name} and I'm currently learning #{language}."
+end
+introduction_with_language = (Makeda, coding)
   
